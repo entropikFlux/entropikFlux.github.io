e5a8451b68683d63095f9b5696f910a140019def
entropikFlux <theresa.d.wolf@gmail.com>
2025-01-07 17:45:37 -0800

    Formating tweaks to the JS file.

--- coursera-html-css-js-for-webdevs/module5-assignment/js/script.js
+++ coursera-html-css-js-for-webdevs/module5-assignment/js/script.js
@@ -80,12 +80,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // *** start ***
 // On first load, show home view
+
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
+
 // *** finish **
 
 
@@ -103,7 +105,6 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
